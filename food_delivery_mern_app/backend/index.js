83de03c9700e391ab65ed82f1ad4b8b20158af51
anoj17 <anojbudathoki17@gmail.com>
2024-04-03 09:19:57 +0545

    add frontend url in cors

--- food_delivery_mern_app/backend/index.js
+++ food_delivery_mern_app/backend/index.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({origin: "https://food-delivery-frontend-2o0u3diml-budhathokianojs-projects.vercel.app/"}))
 
 app.use(express.json({limit: "10mb"}))
 
