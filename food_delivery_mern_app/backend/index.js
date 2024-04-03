91af360c1e94509b614d81ea27a2d1361122f011
anoj17 <anojbudathoki17@gmail.com>
2024-04-03 10:20:34 +0545

    modify port

--- food_delivery_mern_app/backend/index.js
+++ food_delivery_mern_app/backend/index.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(express.json({ limit: "10mb" }))
 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 app.get("/", (req, res) => {
     res.send("server is running")
