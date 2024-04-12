ae8a36406f2d6576e5772a8decfa22777302edbf
anoj17 <anojbudathoki17@gmail.com>
2024-04-12 23:17:47 +0545

    add div in cart

--- food_delivery_mern_app/frontend/src/app/cart/page.tsx
+++ food_delivery_mern_app/frontend/src/app/cart/page.tsx
@@ -13,28 +13,31 @@ const page = () => {
   return (
     <div className='bg-slate-200 flex flex-col px-4 md:px-0 justify-center items-center w-full py-4'>
       <h1 className='text-center py-3 text-3xl md:text-4xl font-bold'>Cart Product <span className='text-red-600'>List</span></h1>
-      {
-        productData[0] ?
-          <div className='w-[330px] md:w-[400px] mx-auto'>
-            {
-              productData[0] && productData.map((item: any) => (
-                <CartItem key={item._id}
-                  id={item._id}
-                  name={item.name}
-                  price={item.price}
-                  category={item.category}
-                  image={item.image}
-                  qty={item.qty}
-                  total={item.total}
+      <div className='justify-center items-center w-full'>
 
-                />
-              ))
-            }
-          </div> :
-          <div className='h-full w-full flex justify-center items-center'>
-            <Image src={empty} alt='emptyImage' />
-          </div>
-      }
+        {
+          productData[0] ?
+            <div className='w-[330px] md:w-[400px]'>
+              {
+                productData[0] && productData.map((item: any) => (
+                  <CartItem key={item._id}
+                    id={item._id}
+                    name={item.name}
+                    price={item.price}
+                    category={item.category}
+                    image={item.image}
+                    qty={item.qty}
+                    total={item.total}
+
+                  />
+                ))
+              }
+            </div> :
+            <div className='h-full w-full flex justify-center items-center'>
+              <Image src={empty} alt='emptyImage' />
+            </div>
+        }
+      </div>
 
     </div>
   )
