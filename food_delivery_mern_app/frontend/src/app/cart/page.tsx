3d41bb6af27b31f861cf584e0c9ec645ba71a523
anoj17 <anojbudathoki17@gmail.com>
2024-04-13 00:02:46 +0545

    add flex in cart

--- food_delivery_mern_app/frontend/src/app/cart/page.tsx
+++ food_delivery_mern_app/frontend/src/app/cart/page.tsx
@@ -11,33 +11,30 @@ const page = () => {
   const productData = useSelector((state: any) => state.product.cartSlice)
   // console.log(productData)
   return (
-    <div className='bg-slate-200 flex flex-col px-4 md:px-44 justify-center items-center w-full py-4'>
-      
-      <div className='flex flex-col justify-center md:w-full items-center'>
-        <h1 className='text-center py-3 text-3xl md:text-4xl font-bold'>Cart Product <span className='text-red-600'>List</span></h1>
-        {
-          productData[0] ?
-            <div className='w-[330px]'>
-              {
-                productData[0] && productData.map((item: any) => (
-                  <CartItem key={item._id}
-                    id={item._id}
-                    name={item.name}
-                    price={item.price}
-                    category={item.category}
-                    image={item.image}
-                    qty={item.qty}
-                    total={item.total}
+    <div className='bg-slate-200 flex flex-col px-4 md:px-0 justify-center items-center w-full py-4'>
+      <h1 className='text-center py-3 text-3xl md:text-4xl font-bold'>Cart Product <span className='text-red-600'>List</span></h1>
+      {
+        productData[0] ?
+          <div className='flex justify-center items-center'>
+            {
+              productData[0] && productData.map((item: any) => (
+                <CartItem key={item._id}
+                  id={item._id}
+                  name={item.name}
+                  price={item.price}
+                  category={item.category}
+                  image={item.image}
+                  qty={item.qty}
+                  total={item.total}
 
-                  />
-                ))
-              }
-            </div> :
-            <div className='h-full w-full flex justify-center items-center'>
-              <Image src={empty} alt='emptyImage' />
-            </div>
-        }
-      </div>
+                />
+              ))
+            }
+          </div> :
+          <div className='h-full w-full flex justify-center items-center'>
+            <Image src={empty} alt='emptyImage' />
+          </div>
+      }
 
     </div>
   )
